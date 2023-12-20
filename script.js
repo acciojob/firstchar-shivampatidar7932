2d96687a6d8fea6520770454d7393c9b5b229323
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-12-20 14:59:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstChar(text) {
-  // your code here
+    // Your code here
+    for (let char of text) {
+        if (char !== ' ') {
+            return char;
+        }
+    }
+    return ''; // Return empty string if the input string only contains spaces
 }
 
 // Do not change the code below
